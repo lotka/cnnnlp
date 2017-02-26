38495688ccd4a92e31ceed7e162504c9b7dd2af5
Luka Milic <lukam321@gmail.com>
2017-02-26 23:16:08 +0000

    added training code

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # cnnnlp
 Using CNNs for text classification
 
+[2] was the starting point for the code
+
 
 # References
-* Convolutional Neural Networks for Sentence Classification - https://arxiv.org/abs/1408.5882
-* Implementing a CNN for Text Classification in TensorFlow - http://www.wildml.com/2015/12/implementing-a-cnn-for-text-classification-in-tensorflow/
-* Good literature overview: https://opendatascience.com/blog/understanding-convolutional-neural-networks-for-nlp/
+[1] Convolutional Neural Networks for Sentence Classification - https://arxiv.org/abs/1408.5882
+[2] Implementing a CNN for Text Classification in TensorFlow - http://www.wildml.com/2015/12/implementing-a-cnn-for-text-classification-in-tensorflow/
+[3] Good literature overview: https://opendatascience.com/blog/understanding-convolutional-neural-networks-for-nlp/
---
+++ data_helpers.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import re
+import itertools
+from collections import Counter
+import pandas as pd
+import os
+import pandas as pd
+import re
+import os
+
+def clean_str_simple(string):
+    for c in '!"£$%^&*()_+{}:@~<>?|`-=[];\'\#\,\.\/\\':
+        string = string.replace(c,'')
+    return string.lower()
+
+def load_book(book_path,max_sentence_length,encoding='binary'):
+    df = pd.read_csv(book_path,delimiter='\t',header=None)
+
+    # name the columns
+    df.columns =  ['score','url','title','text']
+    # drop the url
+    df = df.drop('url',axis=1)
+    # remove the html tags
+    df.text = df.text.str.replace('<(.*?)>','')
+    df.title = df.title.str.replace('<(.*?)>','')
+
+    # clean up text
+    df['title'] = df.title.apply(clean_str_simple).apply(lambda x : ' '.join(x.split(' ')[:max_sentence_length]))
+    df['text'] = df.text.apply(clean_str_simple).apply(lambda x : ' '.join(x.split(' ')[:max_sentence_length]))
+    df['onehot'] = encode_onehot(df.score,method=encoding)
+    df = df[df.onehot.notnull()]
+
+    return df
+
+def load_books(path,books,max_sentence_length,balance_classes=False,encoding='full'):
+    dfs = []
+    for book in books:
+        dfs.append(load_book(os.path.join(path,book),
+                             max_sentence_length=max_sentence_length,
+                             encoding=encoding))
+    return pd.concat(dfs)
+
+
+def encode_onehot(series,method='all'):
+    """ encode a series into onehot format
+
+        Args:
+            series (pd.Series) : a pandas series with hopefully only a few distinct values
+
+        Returns:
+            pd.Series: series of lists
+    """
+    if method == 'full':
+        # discover the categories
+        categories = pd.np.sort(series.unique())
+        # create onehot encoding
+        return series.apply(lambda x : (categories == x).astype(int).tolist())
+    if method == 'binary':
+        def f(x):
+            if x <= 2:
+                return np.array([1,0])
+            elif x >= 4:
+                return np.array([0,1])
+            else:
+                return pd.np.nan
+
+        return series.apply(f)
+
+def load_data(max_sentence_length,
+              path='csv',
+              balance_classes=True,
+              shuffle=True,
+              encoding='binary'):
+    books = ['EL-James-Fifty-Shades-of-Grey.csv',
+             'Andy-Weir-The-Martian.csv',
+             'Donna-Tartt-The-Goldfinch.csv',
+             'Fillian_Flynn-Gone_Girl.csv',
+             'John-Green-The-Fault-in-our-Stars.csv',
+             'Laura-Hillenbrand-Unbroken.csv',
+             'Paula_Hawkins-The-Girl-On-The-Train.csv',
+             'Suzanne-Collins-The-Hunger-Games.csv']
+
+    df = load_books(path,books,encoding=encoding,max_sentence_length=max_sentence_length)
+
+    if balance_classes:
+        max_class_count = df.score.value_counts().loc[1]
+
+        df_list = []
+        for score,group in df.groupby(by='score'):
+            df_list.append(group.iloc[:max_class_count])
+
+        df = pd.concat(df_list).reset_index(drop=True)
+
+    if shuffle:
+        np.random.seed(10)
+        df = df.reindex(np.random.permutation(df.index)).reset_index(drop=True)
+
+    return df
+
+def dataframe_to_xy(df):
+    return df.text.values,np.vstack(df.onehot.values)
+
+def batch_iter(data, batch_size, num_epochs, shuffle=True):
+    """
+    Generates a batch iterator for a dataset.
+    """
+    data = np.array(data)
+    data_size = len(data)
+    num_batches_per_epoch = int((len(data)-1)/batch_size) + 1
+    for epoch in range(num_epochs):
+        # Shuffle the data at each epoch
+        if shuffle:
+            shuffle_indices = np.random.permutation(np.arange(data_size))
+            shuffled_data = data[shuffle_indices]
+        else:
+            shuffled_data = data
+        for batch_num in range(num_batches_per_epoch):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, data_size)
+            yield shuffled_data[start_index:end_index]
